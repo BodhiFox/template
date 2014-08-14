79c9505f1e8e50a9e801b67988538d7ea1d802a6
kabiondi <kellenbiondi@gmail.com>
2014-08-14 15:46:22 -0700

    Incorporated Cycle2 API

--- kellen/prjctr-pull-pics.js
+++ kellen/prjctr-pull-pics.js
@@ -1,4 +1,14 @@
 
+function tagSubmit () {
+				var input = document.getElementById('inputBox');
+				tag = input.value;
+
+				getPics();
+				//slideshow();
+				return false;//prevents refresh on form submit
+			};
+
+
 function getPics(instagram_data) {
 	// Concatenates Instagram url to use as API endpoint and gets JSON-encoded data from Instagram server
 	// uses tag and count from PRJCTR-begin-state.js
@@ -11,16 +21,21 @@ function arrayInit(instagram_data) {
 	// Puts the pics in an array and appends them to the DOM with class and id
 	if (instagram_data.meta.code == 200) {
 		var photos = instagram_data.data;
-		console.log(photos);
 
 		if (photos.length > 0) {
 
 			var i = 0;
+			var picArray = [];
+
 			for (var key in photos) {
 				var photo = photos[key];
-					$('#target').append('<div class="hidden" id='+i+'> <a href="#"><img src=' + photo.images.standard_resolution.url + '></a></div>');
-					i++
+				picArray.push('<img src=' + photo.images.standard_resolution.url + '>');
+				$('.cycle-slideshow').cycle('add', picArray[i]);
+				i++
 			}
+			// for (var i=0; i < picArray.length; i++) {
+		 //        $('.cycle-slideshow').cycle('add', picArray[i]);
+		 //    }
 		}
 		else {
 			$('#target').append("No photos were found");
@@ -30,23 +45,23 @@ function arrayInit(instagram_data) {
 		var error = instagram_data.meta.error_message;
 		$('#target').append('Request Error:' + error);
 	}
-	unhidePics();
+	//unhidePics();
 }
 
-function unhidePics () {
-	// Hides currently visible pic in array, then unhides the next pic
-	if (currentPicId !== 0) {
-		var picToHide = document.getElementById(currentPicId-1);
-		picToHide.setAttribute('class', 'hidden');
-	}
-	var picToShow = document.getElementById(currentPicId);
-	picToShow.setAttribute('class', 'visible');
-	currentPicId++;
-}
+// function unhidePics () {
+// 	// Hides currently visible pic in array, then unhides the next pic
+// 	if (currentPicId !== 0) {
+// 		var picToHide = document.getElementById(currentPicId-1);
+// 		picToHide.setAttribute('class', 'hidden');
+// 	}
+// 	var picToShow = document.getElementById(currentPicId);
+// 	picToShow.setAttribute('class', 'visible');
+// 	currentPicId++;
+// }
 
-function slideshow() {
-	setInterval(unhidePics, 5000);
-}
+// function slideshow() {
+// 	setInterval(unhidePics, 5000);
+// }
 
 
 
